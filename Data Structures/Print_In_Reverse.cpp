6a613a41f0d082585d53558705c125d5c0eef1a7
riskiilyas <riskiilyas03@gmail.com>
2022-03-12 17:23:03 +0700

    add more

--- Data Structures/Print_In_Reverse.cpp
+++ Data Structures/Print_In_Reverse.cpp
@@ -99,3 +99,28 @@ void reversePrint(SinglyLinkedListNode* llist) {
 }
 
 int main()
+{
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        reversePrint(llist->head);
+    }
+
+    return 0;
+}
